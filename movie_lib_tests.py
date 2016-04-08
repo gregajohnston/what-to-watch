2908538771b74a6ae55eb9e61bb32d91026c5391
Greg Johnston <greg.a.johnston@gmail.com>
2016-04-08 12:08:14 -0400

    Added blank classes user, user library and initial tests.

--- movie_lib_tests.py
+++ movie_lib_tests.py
@@ -1,5 +1,7 @@
 from movie import Movie
 from movie_lib import MovieLib
+from user import User
+from user_lib import UserLib
 from nose.tools import raises
 
 mov_lib = MovieLib
@@ -14,3 +16,16 @@ def test_formate_from_item_file():
     based on the data in them"""
     assert mov_lib.read_from_item_file()[0].id == 1
     assert mov_lib.read_from_item_file()[0].genre[0] == 0
+
+
+u_lib = UserLib
+def test_read_from_user_file():
+    """read_from_item_file takes no input, but reads from the csv file named
+    'u.user' in the MovieLens database and returns a list"""
+    assert type(u_lib.read_from_item_file()) == list
+
+def test_format_from_user_file():
+    """read_from_item_file takes no input, but reads from the csv file named
+    'u.user' in the MovieLens database and returns a list"""
+    assert u_lib.read_from_item_file()[0].id == 1
+    assert u_lib.read_from_item_file()[0].genre[0] == 0
---
+++ user_lib.py
@@ -0,0 +1,2 @@
+class UserLib:
+    pass
